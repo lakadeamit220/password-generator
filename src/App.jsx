d32077379a30976c751154e4a79a3bd43616006f
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-07-22 14:38:53 +0530

    Initial commit with project setup and tailwind CSS installation.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl text-center font-bold text-green-500">
+        Password Generator
+      </h1>
+    </>
+  );
+}
+
+export default App;
